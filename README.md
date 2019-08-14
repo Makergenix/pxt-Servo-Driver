2f44a8d56e6638640a10a76069853f56012c55d7
sbcshop <gajender.singh@sb-components.co.uk>
2019-08-14 14:20:41 +0530

    This is the new commmit

---
+++ README.md
@@ -0,0 +1,16 @@
+# micro:bit Servo Driver 
+
+Extension for PCA9685 Servo control for Micro:bit
+
+## License
+
+GNU
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+```package
+Servo=github.com/
+```
---
+++ servo_driver.ts
@@ -0,0 +1,129 @@
+
+/**
+* makecode Servo Driver micro:bit
+* From Makergenix
+* http://www.micropython.org.cn
+*/
+
+enum channels {
+        '0' = 0,
+        '1' = 1,
+        C = 2,
+        D = 3,
+        E = 4,
+        Channel_5 = 5,
+        Channel_6 = 6,
+        Channel_7 = 7,
+        Channel_8 = 8,
+        Channel_9 = 9,
+        Channel_10 = 10,
+        Channel_11 = 11,
+        Channel_12 = 12,
+        Channel_13 = 13,
+        Channel_14 = 14,
+        Channel_15 = 15
+}
+
+
+/**
+ * Custom Blocks
+ */
+//% weight=4 color=#0fbc11 icon="S" block="Servo Driver"
+namespace Servo {
+    const PCA9685_ADDRESS = 0x40
+    const MODE1 = 0x00
+    const MODE2 = 0x01
+    const PRESCALE = 0xFE
+    const LED0_ON_L = 0x06
+    const LED0_ON_H = 0x07
+
+    let initialized = false
+
+    
+
+    function i2cwrite(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2cread(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
+
+    function initPCA9685(): void {
+        i2cwrite(PCA9685_ADDRESS, MODE1, 0x00)
+        setFreq(50);
+        setPwm(0, 0, 4095);
+        for (let idx = 1; idx < 16; idx++) {
+            setPwm(idx, 0, 0);
+        }
+        initialized = true
+    }
+
+    function setFreq(freq: number): void {
+        // Constrain the frequency
+        let prescaleval = 25000000;
+        prescaleval /= 4096;
+        prescaleval /= freq;
+        prescaleval -= 1;
+        let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
+        let oldmode = i2cread(PCA9685_ADDRESS, MODE1);
+        let newmode = (oldmode & 0x7F) | 0x10; // sleep
+        i2cwrite(PCA9685_ADDRESS, MODE1, newmode); // go to sleep
+        i2cwrite(PCA9685_ADDRESS, PRESCALE, prescale); // set the prescaler
+        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode);
+        control.waitMicros(5000);
+        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode | 0xa1);
+    }
+
+    function setPwm(channel: number, on: number, off: number): void {
+        if (channel < 0 || channel > 15)
+            return;
+
+        let buf = pins.createBuffer(5);
+        buf[0] = LED0_ON_L + 4 * channel;
+        buf[1] = on & 0xff;
+        buf[2] = (on >> 8) & 0xff;
+        buf[3] = off & 0xff;
+        buf[4] = (off >> 8) & 0xff;
+        pins.i2cWriteBuffer(PCA9685_ADDRESS, buf);
+    }
+
+	/**
+	 * Servo Execute
+	 * @param degree [0-180] degree of servo; eg: 90, 0, 180
+	*/
+
+    //% blockId=Servo_Degree weight=90 blockGap=20
+    //% block="Rotate servo at Channel %channel | to %degree | degrees"
+    //% degree.min=0 degree.max=180
+    export function Servo(channel: channels, degree: number): void {
+		// if (!initialized) {
+        //    initPCA9685();
+        //  }
+		// 50hz: 20,000 us
+        let v_us = (degree * 1800 / 180 + 600); // 0.6 ~ 2.4
+        let value = v_us * 4096 / 20000;
+        setPwm(channel, 0, value);
+    }
+	
+	/**
+	 * Servo Execute
+	 * @param pulse [500-2500] pulse of servo; eg: 1500, 500, 2500
+	*/
+    //% blockId=setServoPulse block="Rotate servo at %channel | with %pulse | pulse duration"
+    //% weight=85
+    //% pulse.min=500 pulse.max=2500
+    export function ServoPulse(channel: channels, pulse: number): void {
+		// if (!initialized) {
+        //    initPCA9685();
+        //  }
+		// 50hz: 20,000 us
+        let value = pulse * 4096 / 20000;
+        setPwm(channel, 0, value);
+    }
+} 
